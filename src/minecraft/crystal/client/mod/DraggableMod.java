313b129e6620a7a0efb7e3d39ba9274ff8049ed6
Mokkup <serbienx@gmail.com>
2023-08-17 20:10:46 +0100

    more mod shit

--- src/minecraft/crystal/client/mod/DraggableMod.java
+++ src/minecraft/crystal/client/mod/DraggableMod.java
@@ -5,10 +5,14 @@ public abstract class DraggableMod extends Mod {
     public int x;
     public int y;
 
-    public int width, height;
+    public float scale;
 
     public DraggableMod(String name, ModCategory category) {
         super(name, category);
+        this.scale = 1;
     }
+
+    public abstract int getWidth();
+    public abstract int getHeight();
 
 }
