11c00be35dffb0512bbeb928b20226e08d82dd07
Mokkup <serbienx@gmail.com>
2023-08-17 19:54:25 +0100

    More mod system stuff + a draggable mod class for later.

---
+++ src/minecraft/crystal/client/mod/DraggableMod.java
@@ -0,0 +1,9 @@
+package crystal.client.mod;
+
+public abstract class DraggableMod extends Mod {
+
+    public DraggableMod() {
+
+    }
+
+}
--- src/minecraft/crystal/client/mod/Mod.java
+++ src/minecraft/crystal/client/mod/Mod.java
@@ -6,6 +6,41 @@ public class Mod {
     public String description;
     public ModCategory category;
 
+    public boolean enabled;
+
+    public Mod(String name, String description, ModCategory category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public void toggle() {
+         this.enabled = !this.enabled;
+        if(enabled) {
+            onEnable();
+        } else {
+            onDisable();
+        }
+    }
+
+    public void onEnable() {}
+    public void onDisable() {}
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public ModCategory getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     enum ModCategory {
         HUD,
