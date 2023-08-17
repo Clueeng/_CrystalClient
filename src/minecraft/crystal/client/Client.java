1458f97f326fb754121999ce0ae4acf0885afb1e
Mokkup <serbienx@gmail.com>
2023-08-17 17:57:44 +0100

    Add a Client class

--- src/minecraft/crystal/client/Client.java
+++ src/minecraft/crystal/client/Client.java
@@ -2,7 +2,15 @@ package crystal.client;
 
 public class Client {
 
-    public Client() {
+    public String name, version;
+    private static Client crystal;
 
+    public Client() {
+        this.name = name;
+        this.version = version;
+    }
+
+    public static Client getCrystal() {
+        return crystal == null ? crystal = new Client() : crystal;
     }
 }
