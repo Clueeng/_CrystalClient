edd6d5fab6e812a59ec8027fe260f57315d448f5
Mokkup <serbienx@gmail.com>
2023-08-17 18:41:45 +0100

    Add a fucking font renderer, slick, a font and Communication class.

---
+++ src/minecraft/Communication.java
@@ -0,0 +1,11 @@
+public class Communication {
+    // add communication here
+
+    /*
+
+
+    slick.jar has been added to the lib folder, make sure to add it to dependencies in intellij
+    - Maritime
+
+     */
+}
--- src/minecraft/crystal/client/Client.java
+++ src/minecraft/crystal/client/Client.java
@@ -1,9 +1,14 @@
 package crystal.client;
 
+import crystal.client.utils.font.CrystalFontRenderer;
+import net.minecraft.util.ResourceLocation;
+
 public class Client {
 
     public String name, version;
     private static Client crystal;
+
+    public CrystalFontRenderer font;
 
     public Client() {
         this.name = "Crystal";
@@ -15,10 +20,15 @@ public class Client {
     }
 
     public void startup() {
+        this.font = new CrystalFontRenderer(
+                new ResourceLocation("crystal/fonts/regular.ttf"),
+                12
+        );
 
+        System.out.println("Debug: Crystal has started.");
     }
 
     public void shutdown() {
-
+        System.out.println("Debug: Crystal has shutdown.");
     }
 }
